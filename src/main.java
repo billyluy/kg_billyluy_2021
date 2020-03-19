58f111218f2a2f2c55e0f1f3cdd5cc089485d5ee
Billy <luybilly5@gmail.com>
2020-03-18 21:13:01 -0400

    One to one string mapping check, working for test cases, returns true or false to stdout

--- src/main.java
+++ src/main.java
@@ -1,2 +1,35 @@
+import java.util.*;
+
 public class main {
+
+    public  static void main(String args[]){
+        String s1 = args[0];
+        String s2 = args[1];
+        if (oneToOneString(s1, s2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
+
+    public static boolean oneToOneString(String s, String t){
+        HashMap<Character,Character> map = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) != t.charAt(i))
+                    return false;
+            }else{
+                if(map.containsValue(t.charAt(i))){
+                    return false;
+                }else{
+                    map.put(s.charAt(i),t.charAt(i));
+                }
+            }
+        }
+        return true;
+    }
 }
